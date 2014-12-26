0f0829da026faacad4e1b858f3809fcc83be3710
jakekaad <kaadalac@gmail.com>
2014-12-26 18:46:06 +0000

    Game initialized.  Username setup, username authentication setup.

--- main.rb
+++ main.rb
@@ -1,7 +1,54 @@
 require 'rubygems'
 require 'sinatra'
+require 'pry'
 
-set :sessions, true
+use Rack::Session::Cookie, :key => 'rack.session',
+                           :path => '/',
+                           :secret => 'set_name' 
+def require_user_name
+  redirect('/') unless has_user_name?
+end
+
+def has_user_name?
+  !!session[:user_name]
+end
+@player =''
+
+
+get '/' do
+  if !@player
+    erb :set_name
+  else
+    redirect '/game'
+  end
+end
+
+post '/set_name' do
+  session[:user_name] = params[:user_name]
+  session[:bank] = 500
+  redirect '/bet'
+end
+
+get '/bet' do
+  require_user_name
+  erb :bet
+end
+
+post '/place_bet' do
+  session[:bet] = params[:bet].to_i
+  if session[:bet] > session[:bank]
+    @error = "Your bet was bigger than your bank, you went all in!"
+    session[:bet] = session[:bank]
+  end
+  session[:bank] -= session[:bet]
+  redirect '/game'
+end
+
+get '/game' do
+  require_user_name
+  erb :game
+end
+
 
 
 
