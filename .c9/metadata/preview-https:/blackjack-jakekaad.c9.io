8e497962c13dd16f16f3ad013b3bb1240dedce2f
jakekaad <kaadalac@gmail.com>
2014-12-27 21:24:20 +0000

    Establish core gameflow

--- .c9/metadata/preview-https:/blackjack-jakekaad.c9.io
+++ .c9/metadata/preview-https:/blackjack-jakekaad.c9.io
@@ -1,1 +1,1 @@
-{"filter":false,"title":"[P] https://blackjack-jakekaad.c9.io","undoManager":{"mark":0,"position":-1,"stack":[]},"preview":{"path":"https://blackjack-jakekaad.c9.io","previewer":"preview.browser"},"timestamp":1419635606034,"hash":"da39a3ee5e6b4b0d3255bfef95601890afd80709"}
+{"filter":false,"title":"[P] https://blackjack-jakekaad.c9.io","undoManager":{"mark":0,"position":-1,"stack":[]},"preview":{"path":"https://blackjack-jakekaad.c9.io","previewer":"preview.browser"},"timestamp":1419714911088,"hash":"da39a3ee5e6b4b0d3255bfef95601890afd80709"}
--- main.rb
+++ main.rb
@@ -15,7 +15,7 @@ def has_user_name?
 end
 
 def create_deck
-  ["Spades", "Clubs", "Hearts", "Diamonds"].product((2..10).to_a << "Jack" << "Queen" << "King" << "Ace").shuffle
+ ((2..10).to_a << "Jack" << "Queen" << "King" << "Ace").product(["Spades", "Clubs", "Hearts", "Diamonds"]).shuffle
 end
 
 def deal_opening_hands
@@ -58,7 +58,32 @@ def evaluate_totals(player_hand, dealer_hand)
   session[:player_total] = calculate_total(player_hand)
   session[:dealer_total] = calculate_total(dealer_hand)
 end
-  
+
+def players_finished?
+  session[:dealer_total] > 17 && session[:dealer_turn]
+end
+
+def start_new_game
+    session[:dealer_turn] = false
+    session[:deck] = create_deck
+    session[:player_hand] = []
+    session[:dealer_hand] = []
+    session[:bet] = 0
+end
+
+def game_over
+  session.clear
+  redirect '/'
+end
+
+def bust?
+  session[:player_total] > 21 || session[:dealer_total] > 21
+end
+
+def blackjack?
+ session[:player_hand].size == 2 && session[:player_total] == 21
+end
+
 get '/' do
   redirect('/bet') if has_user_name?
   erb :set_name
@@ -72,37 +97,65 @@ end
 
 get '/bet' do
   require_user_name
+  start_new_game
   erb :bet
 end
 
 post '/place_bet' do
   session[:bet] = params[:bet].to_i
+  place_bet
   redirect '/game'
 end
 
 get '/game' do
   require_user_name
-  if session[:player_hand].empty?
-    session[:deck] = create_deck
-    session[:player_hand] = []
-    session[:dealer_hand] = []
-    deal_opening_hands
-  end
-  place_bet if session[:bet]
+  deal_opening_hands if session[:player_hand].empty?
   evaluate_totals(session[:player_hand], session[:dealer_hand])
+  redirect '/game_over' if players_finished? #|| bust? || blackjack?
+  redirect '/dealer_turn' if session[:dealer_turn]
   erb :game
 end
 
-post '/hit' do
-  session[:player_hand] << session[:deck].pop
-  redirect '/game'
+post '/hit_or_stay' do
+  if params[:hit_or_stay] == "hit"
+    session[:player_hand] << session[:deck].pop
+    redirect '/game'
+  else
+    session[:dealer_turn] = true
+    redirect '/dealer_turn'
+  end
 end
 
-post '/stay' do
-  
+get '/start_over' do
+  session.clear
+  redirect '/'
 end
 
+get '/dealer_turn' do
+  erb :dealer_turn
+end
 
+get '/game_over' do
+  erb :game_over
+end
+
+post '/dealer_hit' do
+  if session[:dealer_total] < 17
+    session[:new_dealer_card] = session[:deck].pop
+    session[:dealer_hand] << session[:new_dealer_card]
+    redirect '/game'
+  else
+    redirect '/game_over'
+  end
+end
+
+post '/again' do
+  if params[:yes_or_no] == "no"
+    game_over
+  else
+    redirect '/bet'
+  end
+end
 
 
 
