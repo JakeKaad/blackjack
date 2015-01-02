018491cff8638775e925feac25bdac83a0b53e45
Jake Kaad <kaadalac@gmail.com>
2015-01-01 16:01:03 -0800

    Added class selectors

--- public/application.js
+++ public/application.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$(document).on('click', 'form#player_form player_hit_button', function(){
+	$(document).on('click', 'form#player_form #player_hit_button', function(){
 		$.ajax({
 			type: 'POST',
 			url: '/game/player/hit_or_stay'
@@ -8,7 +8,7 @@ $(document).ready(function(){
 		});
 		return false;
 	});
-	$(document).on('click', 'form#dealer_form dealer_hit_button', function(){
+	$(document).on('click', 'form#dealer_form #dealer_hit_button', function(){
 		$.ajax({
 			type: 'POST',
 			url: '/dealer/hit'
